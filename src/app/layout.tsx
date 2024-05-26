473ded96bd77a593cf83394e5b8fc81176176e4c
Emil Kowalczyk <emilkowalczyk0@icloud.com>
2024-05-26 20:32:38 +0200

    add projects

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,29 @@
 import Navbar from '@/components/Navbar';
-import { Inter } from 'next/font/google';
+import {Inter} from 'next/font/google';
 import './globals.css';
 import Footer from '@/components/Footer';
 
 export const metadata = {
-  title: 'InfratechGroup',
-  description: 'Innowacyjne rozwiązania dla twojego domu',
+    title: 'Infratech',
+    description: 'Innowacyjne rozwiązania dla twojego domu',
 };
 
 const inter = Inter({
-  subsets: ['latin'],
+    subsets: ['latin'],
 });
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
+                                       children,
+                                   }: {
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang='pl'>
-      <body className={inter.className} suppressHydrationWarning>
-        <Navbar />
+    return (
+        <html lang='pl'>
+        <body className={inter.className} suppressHydrationWarning>
+        <Navbar/>
         {children}
-        <Footer />
-      </body>
-    </html>
-  );
+        <Footer/>
+        </body>
+        </html>
+    );
 }
