060707abfbeb57bb09b3ee5d3af8c089b6d4a6d4
Emil Kowalczyk <emilkowalczyk0@icloud.com>
2024-05-26 19:42:19 +0200

    add first project

--- src/app/realizacje/architektura-wnetrz/sad-okregowy/page.tsx
+++ src/app/realizacje/architektura-wnetrz/sad-okregowy/page.tsx
@@ -9,7 +9,8 @@ function SadOkregowy() {
         <div className={`grid grid-cols-1 md:grid-cols-3 w-full max-w-7xl mx-auto px-5 gap-5`}>
             {images.map((image, i) => (
                 <div key={i}>
-                    <Image src={image} width={1000} height={0} className={`w-full aspect-square`}/>
+                    <Image src={image} width={1000} height={0} className={`w-full aspect-square`} alt={`image`}
+                           priority/>
                 </div>
             ))}
         </div>
