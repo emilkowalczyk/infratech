71142e42b41bcddafc6e6e0523e51a6241edb5bb
Emil Kowalczyk <emilkowalczyk0@icloud.com>
2024-05-26 20:04:08 +0200

    add projects

--- src/app/realizacje/architektura-wnetrz/page.tsx
+++ src/app/realizacje/architektura-wnetrz/page.tsx
@@ -10,9 +10,34 @@ function ArchitekturaWnetrz() {
                     <div className={`overflow-hidden`}>
                         <Image src="/images/architektura-wnetrz/sad-okregowy/1.jpg" alt="sad okregowy" width={1000}
                                height={0}
+                               priority
                                className="w-full aspect-square object-cover transition duration-300 hover:scale-[1.03]"/>
                         <h3 className='absolute right-0 bottom-4 bg-white px-10 py-3 text-center text-black text-xl'>
                             Koncepcja Czytelni Akt Sąd Okręgowy Warszawa Praga
+                        </h3>
+                    </div>
+                </Link>
+                <Link href="/realizacje/architektura-wnetrz/wizualizacja-salonu" className={`relative`}>
+                    <div className={`overflow-hidden`}>
+                        <Image src="/images/architektura-wnetrz/wizualizacja-salonu/1.jpg" alt="sad okregowy"
+                               width={1000}
+                               height={0}
+                               priority
+                               className="w-full aspect-square object-cover transition duration-300 hover:scale-[1.03]"/>
+                        <h3 className='absolute right-0 bottom-4 bg-white px-10 py-3 text-center text-black text-xl'>
+                            Wizualizacja salonu
+                        </h3>
+                    </div>
+                </Link>
+                <Link href="/realizacje/architektura-wnetrz/wizualizacja-kuchni" className={`relative`}>
+                    <div className={`overflow-hidden`}>
+                        <Image src="/images/architektura-wnetrz/wizualizacja-kuchni/1.jpg" alt="sad okregowy"
+                               width={1000}
+                               height={0}
+                               priority
+                               className="w-full aspect-square object-cover transition duration-300 hover:scale-[1.03]"/>
+                        <h3 className='absolute right-0 bottom-4 bg-white px-10 py-3 text-center text-black text-xl'>
+                            Wizualizacja kuchni
                         </h3>
                     </div>
                 </Link>
---
+++ src/app/realizacje/architektura-wnetrz/wizualizacja-salonu/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Image from "next/image";
+
+function WizualizacjaSalonu() {
+    const images = ['/images/architektura-wnetrz/wizualizacja-salonu/1.jpg', '/images/architektura-wnetrz/wizualizacja-salonu/2.jpg', '/images/architektura-wnetrz/wizualizacja-salonu/3.jpg', '/images/architektura-wnetrz/wizualizacja-salonu/4.jpg', '/images/architektura-wnetrz/wizualizacja-salonu/5.jpg', '/images/architektura-wnetrz/wizualizacja-salonu/6.jpg', '/images/architektura-wnetrz/wizualizacja-salonu/7.jpg', '/images/architektura-wnetrz/wizualizacja-salonu/8.jpg']
+    return (
+        <div className={`grid grid-cols-1 md:grid-cols-3 w-full max-w-7xl mx-auto px-5 gap-5`}>
+            {images.map((image, i) => (
+                <div key={i}>
+                    <Image src={image} width={1000} height={0} className={`w-full aspect-square`} alt={`image`}
+                           priority/>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default WizualizacjaSalonu
